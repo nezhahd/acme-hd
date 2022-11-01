aaa6d77fa240cc43ec847a4797adc407b726b1e3
甬哥侃侃侃ygkkk <renweikg@gmail.com>
2022-11-01 07:06:29 +0000

    Update acme.sh

--- acme.sh
+++ acme.sh
@@ -150,10 +150,10 @@ red "经检测，你正在使用freenom免费域名解析，不支持当前DNS A
 fi
 domainIP=$(curl -s ipget.net/?ip=acme.sh."$ym")
 if [[ -n $(echo $domainIP | grep nginx) ]]; then
-green "经检测，当前为单域名证书申请" && sleep 2
+green "经检测，当前为单域名证书申请，请输入解析好的二级域名" && sleep 2
 domainIP=$(curl -s ipget.net/?ip="$ym")
 else
-green "经检测，当前为泛域名证书申请" && sleep 2
+green "经检测，当前为泛域名证书申请，请直接输入一级主域名，不要出现 * 字样" && sleep 2
 fi
 wro
 echo
@@ -310,10 +310,10 @@ white "甬哥blogger博客 ：ygkkk.blogspot.com"
 white "甬哥YouTube频道 ：www.youtube.com/c/甬哥侃侃侃kkkyg"
 yellow "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" 
 yellow " 提示："
-yellow " 一、standalone模式仅支持单域名证书申请"
-yellow " 二、DNS API模式不支持freenom免费域名申请，支持单域名与泛域名证书申请"
+yellow " 一、独立模式仅支持单域名证书申请"
+yellow " 二、DNS API模式不支持freenom免费域名申请，支持单域名与泛域名证书申请，其中泛域名申请须在解析平上设置一个名称为 * 字符的解析记录"
 echo
-green " 1. acme.sh申请letsencrypt ECC证书（支持standalone模式与DNS API模式） "
+green " 1. acme.sh申请letsencrypt ECC证书（支持独立模式与DNS API模式） "
 green " 2. 查询已申请成功的域名及自动续期时间点；撤销并删除当前已申请的域名证书 "
 green " 3. 手动一键续期或指定续期的域名证书 "
 green " 4. 卸载一键ACME证书申请脚本 "
