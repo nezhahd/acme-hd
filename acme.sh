70da1b77b4b5281c3ee61629ebffbdbd44473af5
甬哥侃侃侃ygkkk <renweikg@gmail.com>
2022-11-04 09:07:22 +0000

    Update acme.sh

--- acme.sh
+++ acme.sh
@@ -280,7 +280,7 @@ bash /root/.acme.sh/acme.sh --list
 acmeshow(){
 if [[ -n $(/root/.acme.sh/acme.sh -v 2>/dev/null) ]]; then
 caacme1=`bash /root/.acme.sh/acme.sh --list | tail -1 | awk '{print $1}'`
-if [[ -n $caacme ]]; then
+if [[ -n $caacme1 ]]; then
 caacme=$caacme1
 else
 caacme='无证书申请记录'
